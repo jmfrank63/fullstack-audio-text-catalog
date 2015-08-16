3343123ce153949eb1b126df48fc0c8e30844032
Johannes Frank <jmfrank63@gmail.com>
2015-08-16 20:43:44 +0000

    added users.py for login

--- project.py
+++ project.py
@@ -6,11 +6,28 @@ from werkzeug.contrib.fixers import ProxyFix
 
 from os import getenv
 
+import users
+
 app = Flask(__name__)
 
 @app.route('/')
-def show_root():
-    return "Hello Audio Text Catalog!"
+@app.route('/languages/')
+def languages():
+    '''
+    This is the main page for all public visible languages.
+    The page does not take any parameters.
+    '''
+    return "Here a list of all languages will be shown"
+
+@app.route('/language/<int:language_id>/')
+def langauge(language_id):
+    '''
+    This page shows the content of a public visible language
+    with id <language_id>
+    '''
+    return "Here a list of all texts of language with id {0}"\
+            " will be shown".format(language_id)
+
 
 if __name__ == '__main__':
     app.secret_key = 'udacity_secret_app_development_key'
