b61bd2e60db3f5d7db696d4d40015dce20d4f6f7
Johannes Frank <jmfrank63@gmail.com>
2015-08-18 20:14:14 +0000

    pep8 compliant project.py

--- project.py
+++ project.py
@@ -4,11 +4,11 @@ Main project flask file for the audio text catalog
 from flask import Flask
 from werkzeug.contrib.fixers import ProxyFix
 
+
 from os import getenv
 
-import users
-
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/languages/')
@@ -19,6 +19,7 @@ def languages():
     '''
     return "Here a list of all languages will be shown"
 
+
 @app.route('/language/<int:language_id>/')
 def langauge(language_id):
     '''
@@ -26,11 +27,11 @@ def langauge(language_id):
     with id <language_id>
     '''
     return "Here a list of all texts of language with id {0}"\
-            " will be shown".format(language_id)
+        " will be shown".format(language_id)
 
 
 if __name__ == '__main__':
     app.secret_key = 'udacity_secret_app_development_key'
     app.debug = True
     app.wsgi_app = ProxyFix(app.wsgi_app)
-    app.run(host=getenv('IP','0.0.0.0'), port=int(getenv('PORT', '8080')))
+    app.run(host=getenv('IP', '0.0.0.0'), port=int(getenv('PORT', '8080')))
