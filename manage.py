b2117babd6948b4a58ce15556d36164afd0c5ffe
Johannes Frank <jmfrank63@gmail.com>
2015-09-10 20:15:35 +0000

    removed run.py

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 ''' Mangager script for flask. '''
 # from flask.ext.script import Manager, Shell, Server
 from flask_script import Manager, Shell, Server
+from werkzeug.contrib.fixers import ProxyFix
+
 from os import getenv
 from atcatalog import app
 
@@ -9,10 +11,13 @@ from atcatalog import app
 # get server address from environment
 IP = getenv('IP', '0.0.0.0')
 PORT = getenv('PORT', '8080')
-
+SECRET = getenv('FLASK_SECRET', 'udacity_super_secret_key')
 
 def main():
     '''The main entry point'''
+    app.secret_key = SECRET
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.debug = True
     manager = Manager(app)
 
     manager.add_command('run', Server(IP, PORT))
