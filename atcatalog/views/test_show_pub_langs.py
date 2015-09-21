aeeea9ef0c0a0518c26cc6331c4ec4a4f658bc76
Johannes Frank <jmfrank63@gmail.com>
2015-09-21 12:29:16 +0100

    change way of testing

---
+++ atcatalog/views/test_show_pub_langs.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from atcatalog import app
+
+__author__ = 'johannesfrank'
+
+
+class TestShow_pub_langs(TestCase):
+    def test_show_pub_langs_statuscode(self):
+        '''
+        Test the return status code.
+        '''
+        self.fail()
