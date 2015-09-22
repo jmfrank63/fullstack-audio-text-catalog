f32a743b514c7db723f464926328d4ade2cbd8a4
Johannes Frank <jmfrank63@gmail.com>
2015-09-22 12:22:30 +0100

    added variables to pubLang rendering

--- atcatalog/views/pub_lang.py
+++ atcatalog/views/pub_lang.py
@@ -2,15 +2,20 @@
 Public lang views
 '''
 from atcatalog import app
-from flask import render_template
+from atcatalog.views.users import login
+from flask import render_template, url_for
 
 
 @app.route('/')
 @app.route('/langs/')
 def show_pub_langs():
     ''' Show all public languages
+    Link to the user login and the content of each language
     '''
-    return render_template('pubLangs.html')
+    languages = {0: 'english', 1: 'spanish', 2: 'french', 3: 'german',
+                 4: 'portuguese'}
+    login_link = url_for(login)
+    return render_template('pubLangs.html', langs=languages, login=login_link)
 
 
 @app.route('/lang/<int:lid>/')
