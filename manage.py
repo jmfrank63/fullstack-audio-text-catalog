1e0e6609d143deb9932ca71f6fb50332a4a2a3d7
Johannes Frank <jmfrank63@gmail.com>
2015-10-01 17:10:52 +0000

    changed manage.py to discover tests

--- manage.py
+++ manage.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 ''' Mangager script for flask. '''
-from flask.ext.script import Manager, Shell, Server
+from flask.ext.script import Manager, Shell, Server, Command
 from werkzeug.contrib.fixers import ProxyFix
 
 from os import getenv, system
 from atcatalog import app
 
+import unittest
 
 # get server address from environment
 IP = getenv('IP', '0.0.0.0')
 PORT = getenv('PORT', '8080')
 SECRET = getenv('FLASK_SECRET', 'udacity_super_secret_key')
 
+class Test(Command):
+    '''
+    Runs the tests
+    '''
+    def run(self):
+        '''
+        Calls the test
+        '''
+        print 'Testing status codes:'
+#        system('python ./atcatalog/views/test_pub_langs.py')
+        testsuite = unittest.TestLoader().discover('./atcatalog/views')
+        unittest.TextTestRunner(verbosity=1).run(testsuite)
 
 def main():
     '''The main entry point'''
@@ -20,14 +33,9 @@ def main():
     app.debug = True
     manager = Manager(app)
 
-    @manager.command
-    def test():
-        ''' Test command '''
-        print "Testing status codes:"
-        system("python ./atcatalog/tests/statcodes.py")
-
     manager.add_command('run', Server(IP, PORT))
     manager.add_command('shell', Shell())
+    manager.add_command('test', Test())
     manager.run()
 
 if __name__ == '__main__':
